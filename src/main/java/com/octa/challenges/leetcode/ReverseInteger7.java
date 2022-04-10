06341801f6c7364bb10d30cb03111155a03f0d57
sung <sung@sung.com>
2022-04-10 15:22:26 -0500

    Starting commit

---
+++ src/main/java/com/octa/challenges/leetcode/ReverseInteger7.java
@@ -0,0 +1,27 @@
+package com.octa.challenges.leetcode;
+
+public class ReverseInteger7 {
+
+    public static void main(String[] args) {
+
+        int i = 121;
+        System.out.println(reverse(i));
+
+    }
+
+    public static int reverse(int x) {
+
+        int orginalx = x;
+        int reverse = 0;
+
+        while(x !=0) {
+            if (reverse > Integer.MAX_VALUE/10 || (reverse == Integer.MAX_VALUE / 10 && x%10 > 7)) return 0;
+            if (reverse < Integer.MIN_VALUE/10 || (reverse == Integer.MIN_VALUE / 10 && x%10 < -8)) return 0;
+            reverse = reverse*10 + x%10;
+            x = x/10;
+        }
+
+        return reverse;
+    }
+
+}
