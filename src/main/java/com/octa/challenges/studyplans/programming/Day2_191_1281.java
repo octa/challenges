8f3002a08cf068f3e0175850c2274bfaffd5ea50
Octa <octa@github.com>
2022-04-12 00:28:35 -0500

    Starting commit

--- src/main/java/com/octa/challenges/studyplans/programming/Day2_191_1281.java
+++ src/main/java/com/octa/challenges/studyplans/programming/Day2_191_1281.java
@@ -11,7 +11,37 @@ public class Day2_191_1281 {
         int result = subtractProductAndSum(num);
         System.out.println(result);
 
+        int n = 00000000000000000000000000001011;
+        int count = hammingWeight(n);
+        System.out.println(count);
+
     }
+
+/*
+Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+Example 1:
+
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+ */
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+        //return n==0? 0: hammingWeight(n/2) + (n&1);
+    }
+
 /*
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 
