5023994d985123ba829d8fd08cfdc149e15fd73b
Octa <octa@github.com>
2022-04-11 23:50:58 -0500

    Starting commit

---
+++ src/main/java/com/octa/challenges/studyplans/programming/Day2_191_1281.java
@@ -0,0 +1,12 @@
+package com.octa.challenges.studyplans.programming;
+
+public class Day2_191_1281 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int subtractProductAndSum(int n) {
+
+    }
+}
