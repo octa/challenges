f7fe40e93c2b8e64df72975fefb2031177afa002
c <octa@github.com>
2022-05-09 03:23:43 -0500

    Starting commit

--- src/main/java/com/octa/challenges/dsalgo/graph/astar/AStarGrid.java
+++ src/main/java/com/octa/challenges/dsalgo/graph/astar/AStarGrid.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 public class AStarGrid {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public Node astar(Node[][] maze, Node start, Node end) {
         Set<Node> open = new HashSet<>();
         Set<Node> closed = new HashSet<>();
@@ -14,7 +24,7 @@ public class AStarGrid {
         while(!open.isEmpty()) {
             Node node = queue.poll();
             open.remove(node);
-
+            closed.add(node);
             printGrid(maze);
 
             if(node.x == end.x && node.y == end.y) {
@@ -23,7 +33,6 @@ public class AStarGrid {
             }
 
             findNeighbours(maze, node, open, closed, queue, end);
-            closed.add(node);
         }
         return null;
     }
@@ -45,20 +54,43 @@ public class AStarGrid {
             //if(adjacentNode.x >= maze.length || adjacentNode.y >= maze.length) continue;
             if(adjacentNode.value == "#") continue;
 
-            if(open.contains(adjacentNode)) {
-                if(adjacentNode.f < node.f) {
+            if(!closed.contains(adjacentNode) && !open.contains(adjacentNode)) {
+                adjacentNode.g = node.g + 1;
+                adjacentNode.h = Math.abs(adjacentNode.x - end.x) + Math.abs(adjacentNode.y - end.y);
+                adjacentNode.f = adjacentNode.g + adjacentNode.h;
+                adjacentNode.prev = node;
+                open.add(adjacentNode);
+                queue.add(adjacentNode);
+            }
+            else {
+                if(node.g+1 < adjacentNode.g) {
+                    adjacentNode.g = node.g + 1;
+                    adjacentNode.h = Math.abs(adjacentNode.x - end.x) + Math.abs(adjacentNode.y - end.y);
+                    adjacentNode.f = adjacentNode.g + adjacentNode.h;
+                    adjacentNode.prev = node;
+                    queue.add(adjacentNode);
+                    if(closed.contains(adjacentNode)) {
+                        closed.remove(adjacentNode);
+                        open.add(adjacentNode);
+                    }
+                }
+            }
 
+            /*if(closed.contains(adjacentNode)) {
+                if(adjacentNode.f < node.f) {
+                    open.add(adjacentNode);
                 }
             }
 
             if(!closed.contains(adjacentNode)) {
                 adjacentNode.g = node.g + 1;
-                adjacentNode.h = ((adjacentNode.x - end.x) * (adjacentNode.x - end.x)) + ((adjacentNode.y - end.y) * (adjacentNode.y - end.y));
+                //adjacentNode.h = (int) Math.sqrt(((adjacentNode.x - end.x) * (adjacentNode.x - end.x)) + ((adjacentNode.y - end.y) * (adjacentNode.y - end.y)));
+                adjacentNode.h = Math.abs(adjacentNode.x - end.x) + Math.abs(adjacentNode.y - end.y);
                 adjacentNode.f = adjacentNode.g + adjacentNode.h;
                 open.add(adjacentNode);
                 queue.add(adjacentNode);
                 adjacentNode.prev = node;
-            }
+            }*/
 
         }
     }
@@ -76,6 +108,7 @@ public class AStarGrid {
 
         // Start obstacles
 
+        maze[0][4].value = "#";
         maze[1][4].value = "#";
         maze[2][4].value = "#";
         maze[3][4].value = "#";
@@ -89,17 +122,23 @@ public class AStarGrid {
         printInitGrid(maze);
 
         Node start = maze[0][0];
-        Node end = maze[8][8];
+        Node end = maze[0][5];
         Node path = new AStarGrid().astar(maze, start, end);
         findPath(path, start, end);
         printEndGrid(maze, path, start, end);
     }
 
     public static void printInitGrid(Node[][] maze) {
-        for(Node[] i: maze) {
-            for(Node a: i) {
-                System.out.print(a.value);
-                System.out.print("\t");
+        for(int i=0; i<maze.length; i++) {
+            for(int j=0; j<maze.length;j++) {
+                if(maze[i][j].value == "X") {
+                    System.out.print(ANSI_RED + maze[i][j].value + ANSI_RESET);
+                    System.out.print("\t");
+                }
+                else {
+                    System.out.print(maze[i][j].value);
+                    System.out.print("\t");
+                }
             }
             System.out.println("");
         }
@@ -107,9 +146,9 @@ public class AStarGrid {
     }
 
     public static void printGrid(Node[][] maze) {
-        for(Node[] i: maze) {
-            for(Node a: i) {
-                System.out.print(a.f);
+        for(int i=0; i<maze.length; i++) {
+            for(int j=0; j<maze.length;j++) {
+                System.out.print(maze[i][j].f);
                 System.out.print("\t");
             }
             System.out.println("");
@@ -139,5 +178,6 @@ public class AStarGrid {
         }
         Collections.reverse(paths);
         paths.stream().forEach(System.out::print);
+        System.out.println("");
     }
 }
